16aadcc73d08232eeeb7760b6d215d549f64ba4c
Advik Abhilash <advik.abhilash2014@outlook.com>
2026-01-10 13:43:29 -0500

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,30 @@
+import { scene } from "./engine.js";
+
+export const mats = [
+    new THREE.MeshStandardMaterial({color: 0x55aa55}),
+    new THREE.MeshStandardMaterial({color: 0x808080}),
+    new THREE.MeshStandardMaterial({color: 0x4b2d13}),
+    new THREE.MeshStandardMaterial({color: 0x228b22}),
+    new THREE.MeshStandardMaterial({color: 0x00aaff, transparent: true, opacity: 0.6}),
+    new THREE.MeshStandardMaterial({color: 0xff4500}),
+    new THREE.MeshStandardMaterial({color: 0xedc9af}),
+    new THREE.MeshStandardMaterial({color: 0xff0000})
+];
+
+export const blocks = [];
+
+export function addBlock(x, y, z, t) {
+    const b = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), mats[t]);
+    b.position.set(Math.round(x), Math.round(y), Math.round(z));
+    b.userData.type = t;
+    b.castShadow = true;
+    b.receiveShadow = true;
+    scene.add(b);
+    blocks.push(b);
+    return b;
+}
+
+export function removeBlock(b) {
+    scene.remove(b);
+    blocks.splice(blocks.indexOf(b), 1);
+}
