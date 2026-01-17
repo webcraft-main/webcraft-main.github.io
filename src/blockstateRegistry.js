26c044ad68d1ce004bb515a8e26111b557770191
Advik Abhilash <advik.abhilash2014@outlook.com>
2026-01-17 17:44:05 -0500

    Delete src directory

--- src/blockstateRegistry.js
+++
@@ -1,9 +0,0 @@
-export const blockstates = new Map();
-
-export function registerBlockstate(name, json) {
-    blockstates.set(name, json);
-}
-
-export function getBlockstate(name) {
-    return blockstates.get(name);
-}
