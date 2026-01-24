7f668e6038877df8fc34d5e786b4768b5a72294e
microbit_coder <157979538+microbitcoder652@users.noreply.github.com>
2026-01-24 11:58:54 -0500

    Change loadTexture to async function
    Refactor loadTexture to be asynchronous using Promises.

--- textureLoader.js
+++ textureLoader.js
@@ -2,11 +2,14 @@ const THREE = window.THREE;
 
 export const textureCache = new Map();
 
-export function loadTexture(path) {
+export async function loadTexture(path) {
     if (textureCache.has(path)) return textureCache.get(path);
 
     const loader = new THREE.TextureLoader();
-    const tex = loader.load(path);
+    const tex = await new Promise(resolve => {
+        loader.load(path, resolve);
+    });
+
     tex.magFilter = THREE.NearestFilter;
     tex.minFilter = THREE.NearestFilter;
 
